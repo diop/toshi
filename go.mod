2fa6de8e9428c0a3772bb697fa42b49d60ad8f28
diop <fodesdiop@gmail.com>
2019-02-12 22:15:15 -0800

    add plivo dependency and env variables

--- go.mod
+++ go.mod
@@ -5,10 +5,14 @@ require (
 	github.com/facebookgo/grace v0.0.0-20180706040059-75cf19382434
 	github.com/facebookgo/httpdown v0.0.0-20180706035922-5979d39b15c2 // indirect
 	github.com/facebookgo/stats v0.0.0-20151006221625-1b76add642e4 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.2.8 // indirect
 	github.com/mattn/go-colorable v0.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/plivo/plivo-go v4.0.5+incompatible
+	github.com/sirupsen/logrus v1.3.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v0.0.0-20170224212429-dcecefd839c4 // indirect
 	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,71 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/facebookgo/grace/gracehttp"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
+	plivo "github.com/plivo/plivo-go"
 )
 
+// Handlers
+func replyToMessage(c echo.Context) error {
+	plivoAuthId := os.Getenv("PLIVO_AUTH_ID")
+	plivoAuthToken := os.Getenv("PLIVO_AUTH_TOKEN")
+	client, err := plivo.NewClient(plivoAuthId, plivoAuthToken, &plivo.ClientOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	response, err := client.Messages.Create(plivo.MessageCreateParams{
+		Src:  "+14155696002",
+		Dst:  "+17025305234",
+		Text: "Toshitext just sent some crypto 😎!",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Response: %#v\n", response)
+	return c.String(http.StatusOK, "We have successfully sent the message!")
+}
+
+func receiveMessage(c echo.Context) error {
+	return c.String(http.StatusOK, "The Toshitext service is listening to your commands.")
+}
+
+func getHelp(c echo.Context) error {
+	return c.String(http.StatusOK, "Here's the list of available commands:...")
+}
+
+func renderHome(c echo.Context) error {
+	return c.String(http.StatusOK, "Toshitext v1 - Send crypto with a text message.")
+}
+
+func getBalance(c echo.Context) error {
+	return c.String(http.StatusOK, "Here's your wallet total balance: $100")
+}
+
+func createWallet(c echo.Context) error {
+	return c.String(http.StatusOK, "Your Toshitext wallet has been created. Here's you wallet address and current balance.")
+}
+
+func getWalletAddress(c echo.Context) error {
+	return c.String(http.StatusOK, "Here's your wallet deposit address: 0x50Ebe9ad50DCf1Be1A35570E29587fa9F6eCDB46")
+}
+
 func main() {
+	// Initialize godotenv for reading secrets stored in .env files.
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	e := echo.New()
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -17,9 +74,14 @@ func main() {
 
 	// Set up Echo, configure server side validation, and hook into middleware.
 	e.Server.Addr = ":" + port
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello from Toshi!")
-	})
+
+	// Routes
+	e.GET("/", renderHome)
+	e.POST("/help", getHelp)
+	e.POST("/messages", receiveMessage)
+	e.GET("/wallets", getWalletAddress)
+	e.POST("/wallets", createWallet)
+	e.GET("/users", getBalance)
 
 	// Gracefully shut down the server on interrupt.
 	e.Logger.Fatal(gracehttp.Serve(e.Server))
