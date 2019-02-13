713b8bfe15c2fdd3af4c4b462e8a9be2f12f37f7
diop <fodesdiop@gmail.com>
2019-02-12 23:26:58 -0800

    wallet wip

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/diop/toshi
 
 require (
+	github.com/ethereum/go-ethereum v1.8.22
 	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
 	github.com/facebookgo/grace v0.0.0-20180706040059-75cf19382434
 	github.com/facebookgo/httpdown v0.0.0-20180706035922-5979d39b15c2 // indirect
@@ -17,5 +18,5 @@ require (
 	github.com/sirupsen/logrus v1.3.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v0.0.0-20170224212429-dcecefd839c4 // indirect
-	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67 // indirect
+	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67
 )
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
+// Code attribution: https://github.com/miguelmota/ethereum-development-with-go-book/blob/master/code/generate_wallet.go
 package main
 
 import (
+	"crypto/ecdsa"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/facebookgo/grace/gracehttp"
 	"github.com/labstack/echo"
 	plivo "github.com/plivo/plivo-go"
+	"golang.org/x/crypto/sha3"
 )
 
 type message struct {
@@ -59,7 +65,31 @@ func getBalance(c echo.Context) error {
 }
 
 func createWallet(c echo.Context) error {
-	return c.String(http.StatusOK, "Your Toshitext wallet has been created. Here's you wallet address and current balance.")
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println(hexutil.Encode(privateKeyBytes)[2:]) // fad9c8855b740a0b7ed4c221dbad0f33a83a49cad6b3fe8d5817ac83d38b6a19
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	fmt.Println(hexutil.Encode(publicKeyBytes)[4:]) // 9a7df67f79246283fdc93af76d4f8cdd62c4886e8cd870944e817dd0b97934fdd7719d0810951e03418205868a5c1b40b192451367f28e0088dd75e15de40c05
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println(address) // 0x96216849c49358B10257cb55b28eA603c874b05E
+
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(publicKeyBytes[1:])
+	fmt.Println(hexutil.Encode(hash.Sum(nil)[12:])) // 0x96216849c49358b10257cb55b28ea603c874b05e
+
+	return c.String(http.StatusOK, "Here's your wallet deposit address: 0x50Ebe9ad50DCf1Be1A35570E29587fa9F6eCDB46")
 }
 
 func getWalletAddress(c echo.Context) error {
@@ -76,7 +106,7 @@ func main() {
 	// Set up Echo, configure server side validation, and hook into middleware.
 	e.Server.Addr = ":" + port
 
-	// Taking in JSON and mapping it to our data structure.
+	// Taking in mock JSON and mapping it to our data structure.
 	jsn := `[{"ID":"id","Sender":"sender","MessageTime":"messageTime","Receiver":"receiver","Text":"text"}]`
 
 	// MDR -> Message Detail Records
